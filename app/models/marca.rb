38cbb6934412e3e9ee976b5cc5422b169983d936
Gustavo Aragao <gustavo@couggi.com>
2012-07-24 23:34:11 -0300

    Ajustes no Painel Geral

--- app/models/marca.rb
+++ app/models/marca.rb
@@ -18,11 +18,12 @@ class Marca < ActiveRecord::Base
     sql  = ' select m.id, m.descricao, ifnull(estoque.qtde_estoque,0) qtde_estoque, ifnull(vendido.qtde_vendido,0) qtde_vendido, '
     sql += ' ifnull(round(((ifnull(vendido.qtde_vendido,0)/(ifnull(estoque.qtde_estoque,0) + ifnull(vendido.qtde_vendido,0))) * 100),1),0) percentual, '
     sql += ' ifnull(estoque.soma_estoque,0) soma_estoque, ifnull(vendido.soma_vendido,0) soma_vendido, comprado.valor total_comprado, '
+    sql += ' (ifnull(vendido.soma_vendido,0) - comprado.valor) lucro, '
     sql += ' round(((ifnull(vendido.soma_vendido,0) - ifnull(comprado.valor,0)) / ifnull(comprado.valor,0) * 100),1) percentual_valor '
     sql += '    from '
     sql += '    (select id, descricao from marcas) m '
     sql += '    left outer join '
-    sql += '    (select marca_id id, sum(valor_minimo) valor '
+    sql += '    (select marca_id id, ifnull(sum(valor_minimo),0) valor '
     sql += '    from produtos '
     sql += '    group by marca_id) comprado '
     sql += '    on comprado.id = m.id '
@@ -34,7 +35,7 @@ class Marca < ActiveRecord::Base
     sql += '    (select m.id, m.descricao, sum(p.valor_vendido) soma_vendido, count(*) qtde_vendido '
     sql += '    from produtos p, marcas m where p.venda_id is not null and p.marca_id = m.id group by m.id) vendido '
     sql += '    on comprado.id = vendido.id '
-    sql += '    order by percentual desc, soma_vendido desc '
+    sql += '    order by lucro desc, percentual desc, soma_vendido desc '
 
     find_by_sql(sql)
   end
